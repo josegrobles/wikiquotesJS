dd1a9888322ea651449008e0b5d92d464ceb4736
Jose Antonio González Robles <josegrobles1997@gmail.com>
2017-01-15 20:15:15 +0100

    Code refactored. ES6 guidelines

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-var request = require('request')
-var cheerio = require('cheerio')
+'use strict'
+let request = require('request')
+let cheerio = require('cheerio')
 
-var final = []
 
-request("https://en.wikiquote.org/wiki/Martin_Luther_King,_Jr.",function(error,response,html){
-  var $ = cheerio.load(html)
-  $('div#mw-content-text').children('ul').children('li').each(function(i, elem) {
-    if($(this).children('i').text() != '') final.push({text: $(this).children('ul').children().first().text(), from:$(this).children('ul').children().last().children('i').text()})
-    else if($(this).children('b').text() != '') final.push({text: $(this).children('b').text(), from:$(this).children('ul').children().last().children('i').text()})
-});
-console.log(final)
-
+request("https://en.wikiquote.org/wiki/Albert_Einstein", function(error, response, html) {
+    let $ = cheerio.load(html)
+    let final = []
+    $('div#mw-content-text').children('ul').children('li').each(function(i, elem) {
+        try {
+            const firstElement = $(this).children().first().get(0).tagName
+            switch (firstElement) {
+                case 'i':
+                    addInformation($(this).children('ul').children().first().text(),$(this).children('ul').children().last().children('i').text(),final)
+                    break
+                case 'b':
+                      addInformation($(this).children('b').text(),$(this).children('ul').children().last().children('i').text(),final)
+                      break
+                default:
+                    break
+            }
+        } catch (ex) {
+          console.log(ex)
+        }
+        console.log(final)
+    });
 })
+
+function addInformation(text,from,array){
+  if(from.length >0 && text.length > 0) array.push({text:text,from:from})
+  else if(from.length === 0) array.push({text:text})
+}
