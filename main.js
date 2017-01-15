774dbeb54ac6bab6f161b44e853eead4bbb4c837
Jose Antonio González Robles <josegrobles1997@gmail.com>
2017-01-15 20:31:50 +0100

    Enhanced way of data extraction

--- main.js
+++ main.js
@@ -8,25 +8,23 @@ request("https://en.wikiquote.org/wiki/Albert_Einstein", function(error, respons
     let final = []
     $('div#mw-content-text').children('ul').children('li').each(function(i, elem) {
         try {
-            const firstElement = $(this).children().first().get(0).tagName
-            switch (firstElement) {
-                case 'i':
-                    addInformation($(this).children('ul').children().first().text(),$(this).children('ul').children().last().children('i').text(),final)
-                    break
-                case 'b':
-                      addInformation($(this).children('b').text(),$(this).children('ul').children().last().children('i').text(),final)
-                      break
-                default:
-                    break
-            }
+            let text = $(this).first().contents().filter(function() {
+                return this.type === 'text';
+            }).text()
+            addInformation(text, $(this).children('ul').children().last().children('i').text(), final)
         } catch (ex) {
-          console.log(ex)
+            console.log(ex)
         }
-        console.log(final)
     });
+    console.log(final)
 })
 
-function addInformation(text,from,array){
-  if(from.length >0 && text.length > 0) array.push({text:text,from:from})
-  else if(from.length === 0) array.push({text:text})
+function addInformation(text, from, array) {
+    if (from.length > 5 && text.length > 10) array.push({
+        text: text,
+        from: from
+    })
+    else if (from.length === 0 && text.length > 10) array.push({
+        text: text
+    })
 }
