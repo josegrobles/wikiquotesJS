1d1c74d0a09673af12f73d9d8cea277d6e4bab77
Jose Antonio González Robles <josegrobles1997@gmail.com>
2017-01-16 21:51:31 +0100

    Why use let when we can use const?

--- main.js
+++ main.js
@@ -6,15 +6,15 @@ var getQuotes = url => {
   return new Promise((fulfill,reject) => {
     request("https://en.wikiquote.org" + url, function(error, response, html) {
         if (error) reject(error)
-        let $ = cheerio.load(html)
+        const $ = cheerio.load(html)
         let final = []
         let count = 0;
         $('div#mw-content-text').children().each(function(i, elem) {
             try {
                 if ($(this).get(0).tagName == 'h2') count++
                     if (count < 2) {
-                        let str = $(this).children('li').text()
-                        let text = str.substr(0, str.search($(this).children('li').children('ul').children().last().text().substr(0, 10)))
+                        const str = $(this).children('li').text()
+                        const text = str.substr(0, str.search($(this).children('li').children('ul').children().last().text().substr(0, 10)))
                         addInformation(text, $(this).children('li').children('ul').children().last().children('i').text(), final)
                     }
             } catch (ex) {
@@ -29,8 +29,8 @@ var getQuotes = url => {
 var getUrl = name => {
   return new Promise((fulfill,reject) => {
     request("https://en.wikiquote.org/w/index.php?title=Special:Search&profile=default&fulltext=Search&search=" + name, function(error, response, html) {
-        let $ = cheerio.load(html)
-        let url = $("div.mw-search-result-heading").children('a').prop('href')
+        const $ = cheerio.load(html)
+        const url = $("div.mw-search-result-heading").children('a').prop('href')
         if (error) reject(err)
         else fulfill(url)
     })
