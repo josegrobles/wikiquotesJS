9e6b00ce0cad079e31b1a394e629290021e8474c
Jose Antonio González Robles <josegrobles1997@gmail.com>
2017-01-16 20:18:33 +0100

    Closer to a final way of gettin quotes

--- main.js
+++ main.js
@@ -2,27 +2,28 @@
 let request = require('request')
 let cheerio = require('cheerio')
 
-
-request("https://en.wikiquote.org/wiki/Albert_Einstein", function(error, response, html) {
-    let $ = cheerio.load(html)
-    let final = []
-    let count = 0;
-    $('div#mw-content-text').children().each(function(i, elem) {
-        try {
-            if ($(this).get(0).tagName == 'h2') count++
-            if(count < 2){
-              let text = ''
-                $(this).children('li').children().each(function(i,elem){
-                    if($(this).get(0).tagName != 'ul')  text+= $(this).text() + ' '
-                })
-                addInformation(text, $(this).children('li').children('ul').children().last().children('i').text(), final)
-          }
-        } catch (ex) {
-            console.log(ex)
-        }
-        console.log(final)
-    });
-})
+function getQuotes() {
+    request("https://en.wikiquote.org/wiki/Albert_Einstein", function(error, response, html) {
+        let $ = cheerio.load(html)
+        let final = []
+        let count = 0;
+        $('div#mw-content-text').children().each(function(i, elem) {
+            try {
+                if ($(this).get(0).tagName == 'h2') count++
+                    if (count < 2) {
+                        let str = $(this).children('li').text()
+                        let where = str.search($(this).children('li').children('ul').children().last().text().substr(0,10))
+                        let text = str.substr(0,where)
+                        addInformation(text, $(this).children('li').children('ul').children().last().children('i').text(), final)
+                    }
+            } catch (ex) {
+                console.log(ex)
+            }
+            console.log(final)
+            return final
+        });
+    })
+}
 
 function addInformation(text, from, array) {
     if (from.length > 5 && text.length > 10) array.push({
@@ -33,3 +34,5 @@ function addInformation(text, from, array) {
         text: text
     })
 }
+
+getQuotes()
