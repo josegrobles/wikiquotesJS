d3727e066debc3f9a22d3f0a19f34b1786bfc602
Jose Antonio González Robles <josegrobles1997@gmail.com>
2017-01-17 20:17:40 +0100

    Added private functions and tests for them

---
+++ test/testInternal.js
@@ -0,0 +1,29 @@
+var chai = require('chai'),
+    chaiAsPromised = require("chai-as-promised"),
+    rewire = require('rewire'),
+    wiki = require('../main.js'),
+    wikiRewire = rewire('../main.js')
+
+chai.use(chaiAsPromised)
+var should = chai.should()
+
+getUrl = wikiRewire.__get__('getUrl')
+
+
+describe('Get URL needed for Quote function',function(){
+  it('Find Fernando Alonso URL',function(){
+    return getUrl('Fernando Alonso').should.eventually.equal('/wiki/Fernando_Alonso')
+  })
+
+  it('Find Martin Luther King Jr. URL',function(){
+    return getUrl('Martin Luther King Jr.').should.eventually.equal('/wiki/Martin_Luther_King,_Jr.')
+  })
+
+  it('Find Albert Einstein URL',function(){
+    return getUrl('Albert Einstein').should.eventually.equal('/wiki/Albert_Einstein')
+  })
+
+  it('Find William Shakespeare URL',function(){
+    return getUrl('William Shakespeare').should.eventually.equal('/wiki/William_Shakespeare')
+  })
+})
